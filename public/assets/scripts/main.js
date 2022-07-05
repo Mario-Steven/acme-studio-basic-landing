0f00577ce7936c829a83bcf9135c7bcd3768fe42
Mario Steven Acevedo Mandujano <u20191e456@upc.edu.pe>
2022-07-05 13:23:50 -0500

    feat(hero): Added new.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,5 +1,5 @@
 $.get("_header.html", contentToInject => {
-   $("#header-placeholder").replaceWith(contentToInject);
+    $("#header-placeholder").replaceWith(contentToInject);
 });
 $.get("_footer.html", contentToInject => {
     $("#footer-placeholder").replaceWith(contentToInject);
